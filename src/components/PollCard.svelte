1492350a96f8469428fe48d824ffc3f1df833421
Mike Lee <mikemunkyu.lee@gmail.com>
2023-07-10 21:21:00 -0400

    Styling tweaks

--- src/components/PollCard.svelte
+++ src/components/PollCard.svelte
@@ -4,20 +4,11 @@
 	export let poll: Poll;
 </script>
 
-<div class="poll-card">
-	<h3>{poll.question}</h3>
-	<ul>
+<div class="border-t border-neutral-300 py-4">
+	<h3 class="font-medium mb-4">{poll.question}</h3>
+	<ul class="flex flex-col gap-y-2">
 		{#each poll.options as option}
-			<li>{option}</li>
+			<li class="border-2 border-neutral-200 px-2 py-2 rounded cursor-pointer">{option}</li>
 		{/each}
 	</ul>
 </div>
-
-<style>
-	.poll-card {
-		border: 1px solid #ccc;
-		border-radius: 4px;
-		padding: 1rem;
-		margin-bottom: 1rem;
-	}
-</style>
